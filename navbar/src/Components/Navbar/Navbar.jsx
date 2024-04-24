39551ba2ba6e87f1b525714a38b12f1cce0f0e67
md-rimon-islam <rimonislam.info@gmail.com>
2024-04-24 16:18:43 +0600

    darknlightnav

--- navbar/src/Components/Navbar/Navbar.jsx
+++ navbar/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './Navbar.css'
 import logo_light from '../../assets/logo-black.png'
-// import logo_dark from '../../assets/logo-light.png'
+import logo_dark from '../../assets/logo-white.png'
 import search_icon_light from '../../assets/search-w.png'
-// import search_icon_dark from '../../assets/search-b.png'
+import search_icon_dark from '../../assets/search-b.png'
 import toggle_light from '../../assets/night.png'
-// import toggle_dark from '../../assets/day.png'
+import toggle_dark from '../../assets/day.png'
 
 
-const Navbar = () => {
+const Navbar = ({theme, setTheme}) => {
+
+    const toogle_mode = ()=>{
+      theme == 'light' ? setTheme ('dark') : setTheme ('light');
+      console.log('clicked');
+    }
   return (
     <div className='navbar'>
-        <img src={logo_light} alt="" className='logo' />
+        <img src={theme == 'light'? logo_light : logo_dark} alt="" className='logo' />
         <ul>
             <li>Home</li>
             <li>All Course</li>
@@ -22,9 +27,9 @@ const Navbar = () => {
         </ul>
         <div className="search-box">
             <input type="text" placeholder='Search' />
-            <img src={search_icon_light} alt="" />
+            <img src={theme == 'light' ? search_icon_light : search_icon_dark} alt="" />
         </div>
-        <img src={toggle_light} alt="" className="toggle-icon" />
+        <img onClick={()=>{toogle_mode()}} src={theme == 'light' ? toggle_light : toggle_dark} alt="" className="toggle-icon" />
     </div>
   )
 }
